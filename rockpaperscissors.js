014271d14354263651aeecf2ead2e1f664597485
Waleed Hussein <62118101+Waleed405@users.noreply.github.com>
2022-12-04 17:31:37 +0200

    play() function and options

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,5 +1,33 @@
 function getComputerChoice() {
-    let options = ['Rock', 'Paper', 'Scissors'];
+    let options = ['rock', 'paper', 'scissors'];
     let choice = options[Math.floor(Math.random() * 3)];
     return choice;
+}
+
+let options = {
+    rock: {
+        strongTo: 'scissors', weakTo: 'paper'
+    },
+    paper: {
+        strongTo: 'rock', weakTo: 'scissors'
+    },
+    scissors: {
+        strongTo: 'paper', weakTo: 'rock'
+    }
+}
+
+function play(playerSelection, computerSelection) {
+
+    if(options[playerSelection].strongTo == computerSelection){
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    }
+
+    else if(options[playerSelection].weakTo == computerSelection){
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+
+    else {
+        return "It's a draw!"
+    }
+    
 }
